04f109383628acfc8f98e2e4975c18ae3b4aba84
Ashton Hauser <ashton.hauser777@gmail.com>
2018-11-15 16:43:46 +0000

    All fixed

--- db/migrations/20181115013834_fix_email_id.js
+++ db/migrations/20181115013834_fix_email_id.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('link', (table) => {
+    table.increments('id');
+    table.integer('poll_id').unsigned();
+    table.foreign('poll_id').references('poll.id');
+    table.string('admin');
+    table.string('submission');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('link');
 };
