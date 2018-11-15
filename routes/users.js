4641760b43c11e8ab3decf0b9aa90028b4b2de6c
Tristan <tristandeering@hotmail.com>
2018-11-15 17:51:02 +0000

    finished renders for get requests

--- routes/users.js
+++ routes/users.js
@@ -5,14 +5,46 @@ const router  = express.Router();
 
 module.exports = (knex) => {
 
+  // router.get("/", (req, res) => {
+  //   knex
+  //     .select("*")
+  //     .from("users")
+  //     .then((results) => {
+  //       res.json(results);
+  //   });
+  // });
+
+  // welcome page
   router.get("/", (req, res) => {
-    knex
-      .select("*")
-      .from("users")
-      .then((results) => {
-        res.json(results);
-    });
+    res.render("index.ejs")
   });
+
+  // create new poll link
+  router.get("/poll/create", (req, res) => {
+    res.render("create.ejs")
+  });
+
+  // display links
+  router.get("poll/:id", (req, res) => {
+    res.render(links.ejs)
+  });
+
+  // admin page
+  router.get("admin/poll/:id", (req, res) => {
+
+    res.render("admin.ejs")
+  })
+
+  // sub page
+  router.get("sub/poll:id", (req, res) => {
+    res.render("sub.ejs")
+  })
+
 
   return router;
 }
+
+
+
+
+
