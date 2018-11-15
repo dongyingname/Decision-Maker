439d57115c2314ca57fba358abe0d118d176b340
Ying Dong <dongyingname@yahoo.com>
2018-11-15 12:22:52 -0700

    Create.ejs: Refactor <script> into app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,32 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
-});
+
+  function createDecision(data) {
+
+    const $decision = $("<textarea>").attr("placeholder", "decision").addClass("decision");
+    return $decision;
+  }
+
+  function append(textarea) {
+    $("#form").append(textarea);
+  }
+  $(document).ready(function () {
+    console.log("docunent ready");
+    $("#add").on("click", function (event) {
+      append(createDecision());
+    });
+  });
+
+
+
+
+
+
+  // $(() => {
+  //   $.ajax({
+  //     method: "GET",
+  //     url: "/api/users"
+  //   }).done((users) => {
+  //     for(user of users) {
+  //       $("<div>").text(user.name).appendTo($("body"));
+  //     }
+  //   });;
+  // });
