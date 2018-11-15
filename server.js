817d64d3431208981ec3ee13caf9d7139ff7de5e
Ashton Hauser <ashton.hauser777@gmail.com>
2018-11-15 00:00:16 +0000

    Added get route

--- server.js
+++ server.js
@@ -43,6 +43,10 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/poll/create", (req, res) => {
+  res.render("create");
+});
+
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
