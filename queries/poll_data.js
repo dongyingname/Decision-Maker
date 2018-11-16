fde982c8817660c78e79f3a3a3b1acacaecba11d
Ashton Hauser <ashton.hauser777@gmail.com>
2018-11-16 20:36:56 +0000

    knex poll_data modified to include question

--- queries/poll_data.js
+++ queries/poll_data.js
@@ -3,11 +3,16 @@ const router = express.Router();
 
 module.exports = (knex) => {
   router.get('/admin/poll/:id/data', (req, res) => {
-    knex('option').select('name', 'value').where('poll_id', req.params.id )
+    knex('option').select('name', 'value')
+    .where('poll_id', req.params.id)
     .then((name_value) => {
-      res.send(name_value);
+      return knex('poll').select('question')
+      .where('id', req.params.id)
+      .then((question) => {
+        res.status(200).send({ question, name_value });
+      });
     })
-    .catch(err => console.error('ERROR', err));
+    .catch(err => console.error('ERROR',  err));
   });
   return router;
 };
--- queries/sub_data.js
+++ queries/sub_data.js
@@ -1,17 +1,17 @@
 // const express = require('express');
 // const router = express.Router();
 //
-// //get the value of each options name and value
-// //go through each of them
-// //where the name is equal to option
+//get the value of each options name and value
+//go through each of them
+//where the name is equal to option
+
+// module.exports = (knex) => {
+//   router.get('/sub/poll/:id', (req, res) => {
+//     knex('option')
+//     .increment('value', VALUE )
+//     .where({ 'name': NAME, 'poll_id': req.params.id });
+//   });
 //
-// // module.exports = (knex) => {
-// //   router.get('/sub/poll/:id', (req, res) => {
-// //     knex('option')
-// //     .increment('value', VALUE )
-// //     .where({ 'name': NAME, 'poll_id': req.params.id });
-// //   });
-// //
-// //   router.put();
-// //   return router;
-// // };
+//   router.put();
+//   return router;
+// };
