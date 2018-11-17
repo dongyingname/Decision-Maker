06d10573fd96a22eafdec537c2a16ebe97dd248f
Ying Dong <dongyingname@yahoo.com>
2018-11-16 17:01:05 -0700

    Trying to add error messages for situations of no input in textareas

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,36 +1,53 @@
-
 //Doesn't need a input. Create a textarea DOM element and return this element
-  function createDecision(data) {
+function createDecision(data) {
 
-    const $decision = $("<textarea>").attr("name", "decision").attr("placeholder", "Enter Option").addClass("decision");
-    return $decision;
-  }
+  const $decision = $("<textarea>").attr("name", "decision").attr("placeholder", "Enter Option").addClass("decision");
+  return $decision;
+}
 //Take a DOM element and append it to a parent container #form
-  function append(textarea) {
-    $("#decisions").append(textarea);
-  }
+function append(textarea) {
+  $("#decisions").append(textarea);
+}
 
-  //When DOM elements are loaded, alert in console "document ready"
-  //When #add button is clicked, append a empty textarea to to the form
-  $(document).ready(function () {
-    console.log("docunent ready");
-    $("#add").on("click", function (event) {
-      append(createDecision());
-    });
+//When DOM elements are loaded, alert in console "document ready"
+//When #add button is clicked, append a empty textarea to to the form
+$(document).ready(function () {
+  console.log("docunent ready");
+  $("#add").on("click", function (event) {
+    append(createDecision());
   });
 
+  $("form").submit(function (event) {
+    
+    const email = $('#email').val();
+    const title = $('#poll_title').val();
+    const decisions = $('.decision').val();
+    // console.log("email", email);
+    // console.log("title", title);
+    // console.log("decision",decisions);
+    const arr = decisions.split(" ");
+    console.log(typeof(arr));
+    console.log(arr);
+
+
+    
+  });
+});
 
 
 
 
 
-  // $(() => {
-  //   $.ajax({
-  //     method: "GET",
-  //     url: "/api/users"
-  //   }).done((users) => {
-  //     for(user of users) {
-  //       $("<div>").text(user.name).appendTo($("body"));
-  //     }
-  //   });;
-  // });
+
+
+
+// $(() => {
+//   $.ajax({
+//     method: "GET",
+//     url: "/api/users"
+//   }).done((users) => {
+//     for(user of users) {
+//       $("<div>").text(user.name).appendTo($("body"));
+//     }
+//   });;
+// });
