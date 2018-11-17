ef36c5166981e9b2a73bcd9769004b8f861aafbc
Ying Dong <dongyingname@yahoo.com>
2018-11-17 14:28:04 -0700

    Merging my newest version post into old post

--- public/scripts/sub.js
+++ public/scripts/sub.js
@@ -9,25 +9,34 @@ $(document).ready(function () {
   });
 
   // Returns path only)
-  $("#sub").click(function(){
+  $("#sub").on('click', function (e) {
+    e.preventDefault();
     const lists = $("#dataItems").find($(".listItem"));
     const points = [];
     const decs = [];
     const url = window.location.pathname.split('/')
-    const id =url[url.length-1];
-  
+    const id = url[url.length - 1];
+
     for (let i = 0; i < lists.length; i++) {
       points.push(lists.length - i);
       decs.push(lists[i].innerHTML);
     }
-    
-    $.ajax("/sub/poll/"+id, {
+
+    $.ajax({
+      url: "/sub/poll/" + id,
       type: 'PUT',
-      data: {points,decs,id},
-      // success: function (res) {
-      //     renderTweets([res]);
-      // }
-  })
+      data: {
+        points,
+        decs,
+        id
+      },
+      success: function (data) {
+        console.log("data success", data);
+      },
+      error: function (error) {
+        console.log("error", error);
+      }
+    })
 
 
 
