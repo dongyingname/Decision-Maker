cf62bdf80aa50dc72f10bad9c29bb1b7febbfdd4
Ying Dong <dongyingname@yahoo.com>
2018-11-17 23:13:46 -0700

    poll_data.js

--- queries/poll_data.js
+++ queries/poll_data.js
@@ -2,23 +2,32 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = (knex) => {
-  router.get('/admin/poll/:id', (req, res) => {
-    knex('option').select('name', 'value')
-    .where('poll_id', req.params.id)
-    .then((name_values) => {
-      return knex('poll').select('question')
-      .where('id', req.params.id)
-      .then((question) => {
-        let templateVars = {
-          'question': question[0].question,
-          'name_values': JSON.stringify(name_values)
-        };
-        res.render('admin.ejs', templateVars);
-        // res.send(templateVars)
-      });
-    })
-    .catch(err => console.error('ERROR',  err));
-  });
+    const templateVars = {};
+    router.get('/admin/poll/:id', (req, res) => {
+        const id = req.params.id;
+        knex('user_name').select("user_name","poll_id")
+          .where('poll_id', id)
+          .then((user_names) => {
+            templateVars["user_name"] = user_names[0].user_name;
+            console.log(templateVars);
+          })
+          .then(function () {
+            knex('option').select('name', 'value')
+              .where('poll_id', id)
+              .then((name_values) => {
+                return knex('poll').select('question')
+                  .where('id', id)
+                  .then((question) => {
+                    templateVars["question"] = question[0].question;
+                    templateVars["name_values"] = JSON.stringify(name_values);
+                    console.log("final",templateVars)
+                    res.render('admin.ejs', templateVars);
+                    // res.send(templateVars)
+                  });
+              })
+          })
+          .catch(err => console.error('ERROR', err));
+        });
 
-  return router;
-};
+      return router;
+    };
