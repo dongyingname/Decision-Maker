1acb9f76b6066336c99d3cbbcf108ef0d2392bef
Ying Dong <dongyingname@yahoo.com>
2018-11-17 22:53:01 -0700

    poll.js line 97: sub page is able to take the user name and send it to put route that store it into database

--- routes/poll.js
+++ routes/poll.js
@@ -87,38 +87,44 @@ module.exports = (knex) => {
     // When selecting table one has to specify name and poll_id in case of repeatition of name
     // in the other polls
     knex.select('email').from('owner').where({
-           "id": id
-         })
-         .then(function (mail) {
-           let email = mail[0].email;
-           sendMail.sendSubmitEmail(email, id)
-         })
-      .then(function(){
+        "id": id
+      })
+      .then(function (mail) {
+        let email = mail[0].email;
+        sendMail.sendSubmitEmail(email, id)
+      })
+      .then(function () {
+        return knex('user_name').insert({
+          "poll_id": id,
+          "user_name": user_name
+        })
+      })
+      .then(function () {
         for (let i = 0; i < points.length; i++) {
-        let add = points[i];
-        knex.select('value', 'name', 'poll_id').from('option').where({
-            "name": decs[i],
-            "poll_id": id
-          })
-          .then((option) => {
-            let value = option[0].value;
-            return knex.select('value', 'name', 'poll_id').from('option').where({
+          let add = points[i];
+          knex.select('value', 'name', 'poll_id').from('option').where({
+              "name": decs[i],
+              "poll_id": id
+            })
+            .then((option) => {
+              let value = option[0].value;
+              return knex.select('value', 'name', 'poll_id').from('option').where({
                 name: decs[i],
                 "poll_id": id
               }).update({
                 "value": Number(value) + Number(add)
               })
-          })
-          }
-        })
-        .catch(err => {
-          console.log('ERROR', err)
-          res.status(500).json({
-            error: err.message
-          });
+            })
+        }
+      })
+      .catch(err => {
+        console.log('ERROR', err)
+        res.status(500).json({
+          error: err.message
         });
-        res.status(200).send();
-    })
-    
+      });
+    res.status(200).send();
+  })
+
   return router;
 };
