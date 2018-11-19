51d7277755f97e95d92fbf87ba2e1af627231af8
Ying Dong <dongyingname@yahoo.com>
2018-11-18 19:50:32 -0700

    sub.js refactor multiple functions

--- public/scripts/sub.js
+++ public/scripts/sub.js
@@ -1,25 +1,32 @@
 "use strict"
 
-// When the DOM nodes are loaded:
-// Attach event listener to #sub button, which is used to do PUT request
-// and trigger css effects of HTML elements
-$(document).ready(function () {
+//get the id that is the last of URL
+const url = window.location.pathname.split('/')
+const id = url[url.length - 1];
 
+
+//Use drag and drop on #data_items
+function dragdrop() {
   const foo = document.getElementById("data_items");
   Sortable.create(foo, {
     group: 'foo',
     animation: 100,
     chosenClass: "sortable_chosen"
   });
+}
 
-  const url = window.location.pathname.split('/')
-  const id = url[url.length - 1];
+//Create a event handler that redirect to "/admin/poll/" + id
+//once #result_link is clicked
+function redirect() {
 
   $("#result_link").on("click", function () {
     window.location.href = "/admin/poll/" + id;
   })
+}
 
-
+//Create a event handler that do a PUT request to  "/sub/poll/" + id
+//once  $("#sub") is clicked
+function put() {
   $("#sub").on('click', function (e) {
     const user_name = $("#user_name").val();
     e.preventDefault();
@@ -46,10 +53,20 @@ $(document).ready(function () {
         $(".container").hide();
         $("#sub").hide();
       },
-
       error: function (error) {
         console.log("error", error);
       }
     })
   });
+}
+
+
+// When the DOM nodes are loaded:
+// Attach event listener to #sub button, which is used to do PUT request
+// and trigger css effects of HTML elements
+$(document).ready(function () {
+  dragdrop();
+  redirect();
+  put();
+
 });
